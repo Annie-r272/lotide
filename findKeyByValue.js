bb602b1ae74382b7f8d52b2f62a64899cab14f53
Annnie Rahmati <beaatris@gmail.com>
2020-06-18 03:50:44 +0000

    Added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function (obj, value) {
+  let keysArr = Object.keys(obj);
+  for (let key of keysArr){
+    let result = undefined;
+    if (obj[key] === value ){
+      result = key;
+      return result;
+    }    
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
